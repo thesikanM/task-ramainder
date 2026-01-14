ee516042d541ba3a1e802664d3fb82c6752499cf
Thesikan M <thesigan.gpm@gmail.com>
2026-01-14 20:17:26 +0530

    Initial commit: Spring Boot Task Reminder Application

---
+++ src/main/java/com/taskreminder/TaskReminderApplication.java
@@ -0,0 +1,9 @@
+package com.taskreminder; 
+import org.springframework.boot.SpringApplication; 
+import org.springframework.boot.autoconfigure.SpringBootApplication; 
+@SpringBootApplication 
+public class TaskReminderApplication { 
+    public static void main(String[] args) { 
+        SpringApplication.run(TaskReminderApplication.class, args); 
+    } 
+} 
